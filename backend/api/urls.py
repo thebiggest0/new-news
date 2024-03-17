ff88c26585c083a7d64c519ddc385b19e46a54df
Cai Chun Yan <caichunyan17@gmail.com>
2024-03-17 10:32:01 -0700

    Add api view to GET category

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('hello/', views.hello_world),
     path('news/', views.news_list),
     # path('news/<int:pk>/', views.news_detail),
+    path('news/<str:category>/', views.selected_news),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -12,6 +12,33 @@ def hello_world(request):
 
 @api_view(['GET'])
 def news_list(request):
-    news = News.objects.all()
+    news = News.objects.all().order_by('-publishDate')
     serializer = NewsSerializer(news, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def selected_news(request, category):
+    news = News.objects.filter(category=category)
+    serializer = NewsSerializer(news, many=True)
+    return Response(serializer.data)
+
+# @api_view(['GET'])
+# def news_detail(request, pk):
+#     news = News.objects.get(news_id=pk)
+#     serializer = NewsSerializer(news, many=False)
+#     return Response(serializer.data)
+
+# @api_view(['POST'])
+# def news_create(request):
+#     serializer = NewsSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(serializer.data)
+
+# @api_view(['POST'])
+# def news_update(request, pk):
+#     news = News.objects.get(news_id=pk)
+#     serializer = NewsSerializer(instance=news, data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(serializer.data)
