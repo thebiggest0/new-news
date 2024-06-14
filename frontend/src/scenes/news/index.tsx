64af5d65bc07b426d4c92432e57055fd0764a919
Cai Chun Yan <caichunyan17@gmail.com>
2024-06-13 20:04:49 -0700

    Update structure of how to implement news

--- frontend/src/scenes/news/index.tsx
+++ frontend/src/scenes/news/index.tsx
@@ -1,23 +1,13 @@
 import React, { useState, useEffect } from 'react';
 
-// Removed type Props = {} since it's not used in this example
+type Props = {
+    state: any;
+};
 
-const News = () => {
-
-    let [state, setState] = useState([]);
-    useEffect(() => {
-        getNews();
-    }, []);
-
-    let getNews = async () => {
-        let response = await fetch('/api/news/')
-        let data = await response.json();
-        setState(data);
-    }
+const News: React.FC<Props> = ({ state }) => {
 
     return (
-        // <div>{state.message}</div> // Rendering the message property of the state
-        <section className='gap-16 bg-gray-20 py-10 md:h-5/6 md:pb-0 pt-20 px-10'>
+        <section className='gap-16 bg-gray-20 py-10 md:h-5/6 w-5/6 md:pb-0 pt-24 px-10'>
             <h1>News</h1>
             <ul>
                 {state.slice(0, 20).map((article: any, index: number) => {
